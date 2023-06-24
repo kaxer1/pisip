6e59e53f8b5884e71044bbff6473baae89e39664
aemerchan <angel.merchan@zimait.com>
2023-06-24 11:43:19 -0500

    Agregar archivos de proyecto.

---
+++ Dominio.Model/Abstractions/IBaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Model.Abstractions
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        // insertar
+        void Add(TEntity entity);
+        // actualizar
+        void Modify(TEntity entity);
+        // eliminar
+        void Delete(TEntity entity);
+        // select * from
+        IEnumerable<TEntity> GetAll();
+        // Buscar por ID
+        TEntity Get(int id);
+    }
+}
---
+++ Infra.DataAccess/Repository/BaseRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio.Model.Abstractions;
+
+namespace Infra.DataAccess.Repository
+{
+    public class BaseRepository<TEntity> : IDisposable, IBaseRepository<TEntity> where TEntity : class
+    {
+        public void Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Modify(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
